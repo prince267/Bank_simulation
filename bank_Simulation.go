e30efa9b3ce5835b3e8d41f760616d3c2e9a76d3
prince267 <prince.kumar267@gmail.com>
2019-04-07 23:44:03 +0530

    Bank Simulation Program

--- bank_Simulation.go
+++ bank_Simulation.go
@@ -1,29 +1,43 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
-func banksimmulation(id int, customers <-chan int, results chan<- int) {
+func banksimmulation(id int, customers <-chan int, results chan<- int, coustomerTime int) {
 
 	for j := range customers {
-		fmt.Println("cashier :", id, " coustomers ", j, " started")
-		time.Sleep(3 * time.Second)
-		fmt.Println("cashier :", id, " coustomers ", j, " stopped")
+		fmt.Println((time.Now()).Format("2006-01-02 15:04:05")+" --> Cashier ", id, ": Customer ", j, " Started")
+		time.Sleep(time.Duration(coustomerTime) * time.Second)
+		fmt.Println((time.Now()).Format("2006-01-02 15:04:05")+" --> Cashier ", id, ": Customer ", j, " Completed")
 		results <- 1
 	}
 }
 
 func main() {
-	customers := make(chan int, 100)
-	results := make(chan int, 100)
-	for c := 1; c <= 4; c++ {
-		go banksimmulation(c, customers, results)
+	numCashier := os.Args[1]
+	numCoustomers := os.Args[2]
+	timePerCustomer := os.Args[3]
+	NoOfcashier, _ := strconv.Atoi(strings.Split(numCashier, "=")[1])
+	NoOfcoustomers, _ := strconv.Atoi(strings.Split(numCoustomers, "=")[1])
+	coustomerTime, _ := strconv.Atoi(strings.Split(timePerCustomer, "=")[1])
+
+	customers := make(chan int, NoOfcoustomers)
+	results := make(chan int, NoOfcoustomers)
+	fmt.Println((time.Now()).Format("2006-01-02 15:04:05") + " --> Bank Simulation Started")
+	for c := 1; c <= NoOfcashier; c++ {
+		go banksimmulation(c, customers, results, coustomerTime)
 	}
-	for j := 1; j <= 6; j++ {
-		customers <- j
+	for cus := 1; cus <= NoOfcoustomers; cus++ {
+		customers <- cus
 	}
 	close(customers)
-	for a := 1; a <= 6; a++ {
+	for res := 1; res <= NoOfcoustomers; res++ {
 		<-results
 	}
+	fmt.Println((time.Now()).Format("2006-01-02 15:04:05") + " --> Bank Simulation Ended")
 }
